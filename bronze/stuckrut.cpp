85d64cd2f0fe4e30c6ba6375c507f9ca69525c75
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2022-09-15 00:26:08 -0400

    Correct stuckrut.cpp

--- bronze/stuckrut.cpp
+++ bronze/stuckrut.cpp
@@ -3,28 +3,37 @@ using namespace std;
 
 int main() {
 	int n; cin >> n;
-	vector<char> dir(n);
-	vector<int> x(n), y(n);
+	vector<array<int, 3>> ncows, ecows;
 	for (int i = 0; i < n; i++) {
-		cin >> dir[i] >> x[i] >> y[i];
+		char dir; int x, y;
+		cin >> dir >> x >> y;
+		if (dir == 'N') ncows.push_back({x, y, i});
+		else if (dir == 'E') ecows.push_back({x, y, i});
+		else return -1;
 	}
 
-	for (int i = 0; i < n; i++) {
-		int cut = INT_MAX;
-		for (int j = 0; j < n; j++) {
-			if (dir[j] != dir[i]) {
-				if (dir[j] == 'E' && y[j] > y[i] && x[j] < x[i]
-						&& (y[j]-y[i]) > (x[i]-x[j])) {
-					cut = min(cut, y[j] - y[i]);
-				} else if (dir[j] == 'N' && x[j] > x[i] && y[j] < y[i]
-						&& (x[j]-x[i]) > (y[i]-y[j])) {
-					cut = min(cut, x[j] - x[i]);
+	sort(ncows.begin(), ncows.end());
+	sort(ecows.begin(), ecows.end(), [](auto &left, auto &right) {
+		return left[1] < right[1];
+	});
+
+	vector<int> dist(n);
+	for (auto &ec : ecows) {
+		for (auto &nc : ncows) {
+			if (nc[0] > ec[0] && nc[1] < ec[1] && !dist[nc[2]]) {
+				if (ec[1] - nc[1] > nc[0] - ec[0]) {
+					dist[nc[2]] = ec[1] - nc[1];
+				} else if (ec[1] - nc[1] < nc[0] - ec[0]) {
+					dist[ec[2]] = nc[0] - ec[0];
+					break;
 				}
 			}
 		}
-		if (cut == INT_MAX) cout << "Infinity\n";
-		else cout << cut << '\n';
 	}
 
+	for (auto el : dist) {
+		if (el) cout << el << '\n';
+		else cout << "Infinity\n";
+	}
 	return 0;
 }
