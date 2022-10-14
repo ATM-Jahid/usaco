90b1a303a04bd0d41db457d5ea6d3ff57531f3da
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2022-10-14 18:35:06 -0400

    Solve load balancing

---
+++ bronze/balancing.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+
+int main() {
+	ifstream cin("balancing.in");
+	ofstream cout("balancing.out");
+
+	int n, tmp; cin >> n >> tmp;
+	vector<pii> xsr(n), ysr(n);
+	for (int i = 0; i < n; i++) {
+		int x, y; cin >> x >> y;
+		xsr[i].first = x; xsr[i].second = y;
+		ysr[i].second = x; ysr[i].first = y;
+	}
+	sort(xsr.begin(), xsr.end());
+	sort(ysr.begin(), ysr.end());
+
+	int ans = n;
+	vector<int> q(4);
+	int a = xsr[0].first;
+	for (int i = 0; i < n; i++) {
+		if (xsr[i].first == a) continue;
+		else {
+			q[1] = i; q[0] = n - i;
+			q[2] = 0; q[3] = 0;
+			int b = ysr[0].first;
+			for (int j = 0 ; j < n; j++) {
+				if (ysr[j].first == b) {
+					if (ysr[j].second <= a) {
+						q[1]--;
+						q[2]++;
+					} else {
+						q[0]--;
+						q[3]++;
+					}
+				} else {
+					ans = min(ans, *max_element(q.begin(), q.end()));
+					b = ysr[j].first; j--;
+				}
+			}
+			a = xsr[i].first;
+		}
+	}
+
+	cout << ans << '\n';
+	return 0;
+}
