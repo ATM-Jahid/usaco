f4bd87b18ac8e9cad873343aaf48ba2e005dbea0
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2023-07-06 16:06:35 -0600

    Solve cow tipping
    I read the problem statement wrong. The untipperator is fixed at
    the upper-left cow.

--- bronze/cowtip.cpp
+++ bronze/cowtip.cpp
@@ -1,20 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+ifstream fin("cowtip.in");
+ofstream fout("cowtip.out");
+
+int N;
+vector<vector<int>> jar;
+
+int iszero(int imax) {
+	for (int i = 0; i <= imax; i++) {
+		for (int j = 0; j < N; j++) {
+			if (jar[i][j] == 1) {
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+void flip(int imax, int jmax) {
+	for (int i = 0; i <= imax; i++) {
+		for (int j = 0; j <= jmax; j++) {
+			if (jar[i][j] == 1) jar[i][j] = 0;
+			else jar[i][j] = 1;
+		}
+	}
+}
+
+void print() {
+	for (auto &row : jar) {
+		for (auto &e : row) fout << e;
+		fout << endl;
+	}
+	fout << endl;
+}
+
 int main() {
-	int N; cin >> N;
-	vector<vector<int>> jar(N, vector<int>(N));
+	fin >> N;
+	jar.resize(N, vector<int>(N));
+
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			char c; cin >> c;
+			char c; fin >> c;
 			jar[i][j] = int(c-'0');
 		}
 	}
 
-	//for (auto &row : jar) {
-	//	for (auto &e : row) cout << e;
-	//	cout << endl;
-	//}
+	int cnt = 0;
+	for (int i = N-1; i >= 0 && !iszero(i); i--) {
+		for (int j = N-1; j >= 0; j--) {
+			if (jar[i][j] == 1) {
+				flip(i, j);
+				cnt++;
+				//print();
+			}
+		}
+	}
+
+	fout << cnt << '\n';
 
 	return 0;
 }
