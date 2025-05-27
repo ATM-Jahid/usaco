7bd8870c2f0dec9ff9ce7c60989722851d2497a3
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2025-05-26 22:50:48 -0400

    Improve magic ship
    Prefix sums can be used for the weather cycle.
    I was pulling my hair out
    because the `pdx[i] = pdx[i-1]` update was inside if-else.
    It doesn't work this way since the horizontal state has to propagate
    even when the wind direction is 'U' or 'D'.
    Be careful about coordinate prefix sums in the future.

--- silver/magicship.cpp
+++ silver/magicship.cpp
@@ -15,20 +15,25 @@ int main() {
 	int n; cin >> n;
 	string s; cin >> s;
 
+	vector<int> pdx(n+1), pdy(n+1);
+	for (int i = 1; i <= n; i++) {
+		pdx[i] = pdx[i-1];
+		pdy[i] = pdy[i-1];
+
+		char c = s[i-1];
+		if (c == 'L') pdx[i]++;
+		else if (c == 'R') pdx[i]--;
+		else if (c == 'U') pdy[i]--;
+		else if (c == 'D') pdy[i]++;
+		else return -1;
+	}
+
 	ll res = first_true(0LL, (ll)1e15, [&](ll guess) {
 		ll dx = x2 - x1, dy = y2 - y1;
+		ll quo = guess / n, rem = guess % n;
 
-		for (int i = 0; i < n; i++) {
-			if (s[i] == 'L') {
-				dx += (guess / n) + (i < (guess % n));
-			} else if (s[i] == 'R') {
-				dx -= (guess / n) + (i < (guess % n));
-			} else if (s[i] == 'U') {
-				dy -= (guess / n) + (i < (guess % n));
-			} else if (s[i] == 'D') {
-				dy += (guess / n) + (i < (guess % n));
-			}
-		}
+		dx += pdx[n] * quo + pdx[rem];
+		dy += pdy[n] * quo + pdy[rem];
 
 		if (abs(dx) + abs(dy) > guess) return false;
 		else return true;
